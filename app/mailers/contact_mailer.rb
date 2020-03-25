a8c591525108faa7363238d60e7771484462b7b3
Clint Farley <clint.farley@hotmail.com>
2020-03-25 17:25:30 +0000

    Added mailer info

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'instructors@upskillcourses.com'
+  default to: 'clint.farley@hotmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
