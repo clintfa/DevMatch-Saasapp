c2c9ed85a8d800ca491477fc3025a5a2bfd515f2
Clint Farley <clint.farley@hotmail.com>
2020-03-30 17:25:55 +0000

    Added server side code for creating subscriptions for pro users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,10 +3,13 @@
 $(document).on('turbolinks:load', function(){
   var theForm = $('#pro_form');
   var submitBtn = $('#form-signup-btn');
+  
   //Set Stripe public key.
   Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
+  
   //When user clicks form submit btn,
   submitBtn.click(function(event){
+    
     //prevent default submission behavior.
     event.preventDefault();
     submitBtn.val("Processing").prop('disabled', true);
